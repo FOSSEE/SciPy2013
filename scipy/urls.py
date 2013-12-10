311e7c6fd6d8cc3a9fe023e0d996e46ef3033d79
hardythe1 <hardy_the1@yahoo.com>
2013-12-11 00:58:37 +0530

    Closing registration & CFP

--- scipy/urls.py
+++ scipy/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from website.views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,9 +11,9 @@ urlpatterns = patterns('',
     # Accounts urls
     url(r'accounts/login/$', 'scipy.views.user_login'),
     url(r'accounts/logout/$', 'scipy.views.user_logout'),
-    url(r'accounts/register/$', 'scipy.views.user_register'),
+    url(r'accounts/register/$', 'website.views.call_for_papers_page', name='call-for-proposals'),
     url(r'accounts/profile/$', 'scipy.views.user_profile'),
-    url(r'accounts/upload-document/$', 'scipy.views.upload_document', name='upload-document'),
+    url(r'accounts/upload-document/$', 'website.views.call_for_papers_page', name='call-for-proposals'),
     # Reusing the admin password reset
     url(r'accounts/password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
     url(r'accounts/password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
